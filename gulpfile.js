8e6c22189aa6e1fc35d677168a3fc7fcc6ca3226
Sancho SAIYIN <saincogt@gmail.com>
2016-07-12 16:02:33 +0930

    install some modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,24 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+// Lint task
+gulp.task('jshint', function () {
+	return gulp.src('js/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('jshint-stylish'));
+	});
 
 gulp.task('default', function () {
 	// default tasks
+
+	return gutil.log("Gulp is running!");
 	});
+
+gulp.task('watch', function() {
+	gulp.watch('js/*.js', ['jshint']);
+});
