d7e6c677a9b2db9908d95d7464b9b3f260938cce
Sancho <saincogt@gmail.com>
2016-08-02 19:34:52 +0930

    revised start screen

--- js/boot.js
+++ js/boot.js
@@ -4,21 +4,24 @@ var bootState = function (game) {
 
 bootState.prototype = {
 	preload: function () {
+		var startScreenLabel = game.add.text(300, 40, 'Last Mile', {font: '50px Times New Roman', fill: '#000'});
+		var loadingLabel = game.add.text(300, 260, 'Loading...', {font: '30px Courier', fill: '#000'});
 		this.game.load.image('play', 'assets/img/play.png');
 	},
 	create: function () {
-		var playImg = this.game.add.image(this.world.width/2, this.world.height/2, 'play');
-		this.stage.backgroundColor = "#FFA";
+		// var playImg = this.game.add.image(this.world.width/2, this.world.height/2, 'play');
+		this.stage.backgroundColor = "#FF0";
 		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 		this.scale.pageAlignHorizontally = true;
 
-		var startButton = this.game.add.button(300, 300, 'play', this.load, this);
+		var startButton = this.game.add.button(300, 300, 'play', this.startPlay, this);
+		startButton.scale.setTo(0.3, 0.3);
 		// this.startButton.anchor.setTo(0.5, 0.5);
-		playImg.scale.setTo(0.1, 0.1);
+		// playImg.scale.setTo(0.1, 0.1);
 		// this.scale.setScreenSize();
 		// this.game.state.start('play');
 	},
-	load: function () {
+	startPlay: function () {
 		this.game.state.start('play');
 	}
 };
