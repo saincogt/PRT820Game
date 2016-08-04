b6a832459bc7a02eb4bf2804e14eca77edfae991
Sancho <saincogt@gmail.com>
2016-08-04 19:44:36 +0930

    Added some images. Changed two states 'boot' and 'play'

--- js/boot.js
+++ js/boot.js
@@ -5,25 +5,39 @@ var bootState = function (game) {
 bootState.prototype = {
 	preload: function () {
 		
-		this.load.image('play', 'assets/img/play.png');
-		this.load.image('background', 'assets/img/menu-background.png');
+		this.load.image('background', 'assets/img/background.jpg');
+		this.load.image('title', 'assets/img/title.png');
+		this.load.image('play', 'assets/img/playMenu.png');
+		this.load.image('score', 'assets/img/scoreMenu.png');
 	},
 	create: function () {
-		// var playImg = this.game.add.image(this.world.width/2, this.world.height/2, 'play');
+
 		var background = this.game.add.image(0, 0, 'background');
-		var startScreenLabel = game.add.text(300, 40, 'Last Mile', {font: '50px Times New Roman', fill: '#000'});
-		var loadingLabel = game.add.text(300, 260, 'Loading...', {font: '30px Courier', fill: '#000'});
+
+		var title = game.add.image(this.world.centerX, this.world.centerY/2, 'title');
+		title.anchor.setTo(0.5, 0.5);
+
+		// var loadingLabel = game.add.text(300, 260, 'Loading...', {font: '30px Courier', fill: '#000'});
+		
+
+		var startButton = this.game.add.button(this.world.centerX, 300, 'play', this.startPlay, this);
+		startButton.scale.setTo(0.8, 0.8);
+		startButton.anchor.setTo(0.5, 0.5);
+
+		var scoreButton = this.game.add.button(this.world.centerX, 360, 'score', this.checkScore, this);
+		scoreButton.scale.setTo(0.8, 0.8);
+		scoreButton.anchor.setTo(0.5, 0.5);
+
 		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 		this.scale.pageAlignHorizontally = true;
-
-		var startButton = this.game.add.button(300, 300, 'play', this.startPlay, this);
-		startButton.scale.setTo(0.3, 0.3);
-		// this.startButton.anchor.setTo(0.5, 0.5);
-		// playImg.scale.setTo(0.1, 0.1);
-		// this.scale.setScreenSize();
-		// this.game.state.start('play');
 	},
 	startPlay: function () {
 		this.game.state.start('play');
+	},
+	checkScore: function () {
+		// this.game.state.start('score');
+	},
+	render: function () {
+		// game.debug.game();
 	}
 };
--- js/play.js
+++ js/play.js
@@ -6,22 +6,37 @@ playState.prototype = {
 	preload: function () {
 		this.load.image('airdrop', 'assets/img/airdrop.png');
 		this.load.image('background', 'assets/img/background.jpg');
-		this.load.image('camp', 'assets/img/camp.png');
+		this.load.image('camp', 'assets/img/camp-1.png');
 		this.load.image('convoy', 'assets/img/convoy.png');
 		this.load.image('food', 'assets/img/food.png');
 		this.load.image('porter', 'assets/img/porter.png');
 		this.load.image('shelter', 'assets/img/shelter.png');
-		this.load.image('warehouse', 'assets/img/warehouse.png');
+		this.load.image('warehouse', 'assets/img/warehouse-1.png');
+		this.load.image('poplulation', 'assets/img/population.png');
+		this.load.image('score', 'assets/img/scoreBoard.png');
+		this.load.image('back', 'assets/img/backArrow.png');
 	},
 	create: function () {
 		this.stage.backgroundColor = "#FFF";
 		this.add.image(0, 0, 'background');
 		var warehouse = this.add.image(this.world.width*7/8, this.world.centerY, 'warehouse');
 		var camp = this.add.image(this.world.width/8, this.world.centerY, 'camp');
+		var population = this.add.image(this.world.width/20, this.world.height/20, 'poplulation');
+		var score = this.add.image(this.world.width*15/20, this.world.height/20, 'score');
+
+		var backArrow = this.add.button(this.world.width*1.5/20, this.world.height*18/20, 'back', this.goBack, this);
+		backArrow.scale.setTo(0.05, 0.05);
+		backArrow.anchor.setTo(0.5, 0.5);
+
+		camp.inputEnabled = true;
+		camp.input.enableDrag();
 		warehouse.anchor.setTo(0.5, 0.5);
 		warehouse.scale.setTo(0.2, 0.2);
 		camp.anchor.setTo(0.5, 0.5);
 		camp.scale.setTo(0.2, 0.2);
+	},
+	goBack: function () {
+		this.game.state.start('boot');
 	}
 };
 
