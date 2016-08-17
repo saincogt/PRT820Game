e6e248eeb642439da4d72a2fe544c78a14123598
Sancho <saincogt@gmail.com>
2016-08-17 11:27:02 +0930

    nothing

--- build/js/main.js
+++ build/js/main.js
@@ -77,56 +77,57 @@ module.exports = Camp;
 var Items = function (game, x, y, frame) {
 	Phaser.Sprite.call(this, game, x, y, frame);
 
-	this.showBox = function () {
+	var group = game.add.group();
+	group.inputEnableChildren = true;
 
-		var group = game.add.group();
-		group.inputEnableChildren = true;
+	var foodBox = group.create(game.world.width*15/20, game.world.height*5.5/20, 'foodBox');
+	foodBox.name = 'foodBox';
+	foodBox.alpha = 0.6;
 
-		var foodBox = group.create(game.world.width*15/20, game.world.height*5.5/20, 'foodBox');
-		foodBox.name = 'foodBox';
-		foodBox.alpha = 0.6;
+	var waterBox = group.create(game.world.width*16/20, game.world.height*5.5/20, 'waterBox');
+	waterBox.name = 'waterBox';
+	waterBox.alpha = 0.6;
 
-		var waterBox = group.create(game.world.width*16/20, game.world.height*5.5/20, 'waterBox');
-		waterBox.name = 'waterBox';
-		waterBox.alpha = 0.6;
+	var medicineBox = group.create(game.world.width*17/20, game.world.height*5.5/20, 'medicineBox');
+	medicineBox.name = 'medicineBox';
+	medicineBox.alpha = 0.6;
 
-		var medicineBox = group.create(game.world.width*17/20, game.world.height*5.5/20, 'medicineBox');
-		medicineBox.name = 'medicineBox';
-		medicineBox.alpha = 0.6;
+	var shelterBox = group.create(game.world.width*18/20, game.world.height*5.5/20, 'shelterBox');
+	shelterBox.name = 'shelterBox';
+	shelterBox.alpha = 0.6;
 
-		var shelterBox = group.create(game.world.width*18/20, game.world.height*5.5/20, 'shelterBox');
-		shelterBox.name = 'shelterBox';
-		shelterBox.alpha = 0.6;
+	// var foodBox = game.add.image(game.world.width*15/20, game.world.height*5.5/20, 'foodBox');
+	foodBox.anchor.setTo(0.5, 0.5);
+	foodBox.scale.setTo(0.4, 0.4);
+	// this.foodBox.inputEnabled = true;
+	foodBox.input.enableDrag();
+	// this.foodBox.events.onDragStop.add(this.addFood);
 
-		// var foodBox = game.add.image(game.world.width*15/20, game.world.height*5.5/20, 'foodBox');
-		foodBox.anchor.setTo(0.5, 0.5);
-		foodBox.scale.setTo(0.4, 0.4);
-		// foodBox.inputEnabled = true;
-		foodBox.input.enableDrag();
+	// var waterBox = game.add.image(game.world.width*16/20, game.world.height*5.5/20, 'waterBox');
+	waterBox.anchor.setTo(0.5, 0.5);
+	waterBox.scale.setTo(0.4, 0.4);
+	// waterBox.inputEnabled = true;
+	waterBox.input.enableDrag();
+	// this.waterBox.events.onDragStop.add(this.addWater);
 
-		// var waterBox = game.add.image(game.world.width*16/20, game.world.height*5.5/20, 'waterBox');
-		waterBox.anchor.setTo(0.5, 0.5);
-		waterBox.scale.setTo(0.4, 0.4);
-		// waterBox.inputEnabled = true;
-		waterBox.input.enableDrag();
+	// var medicineBox = game.add.image(game.world.width*17/20, game.world.height*5.5/20, 'medicineBox');
+	medicineBox.anchor.setTo(0.5, 0.5);
+	medicineBox.scale.setTo(0.4, 0.4);
+	// medicineBox.inputEnabled = true;
+	medicineBox.input.enableDrag();
+	// this.medicineBox.events.onDragStop.add(this.addMedicine);
 
-		// var medicineBox = game.add.image(game.world.width*17/20, game.world.height*5.5/20, 'medicineBox');
-		medicineBox.anchor.setTo(0.5, 0.5);
-		medicineBox.scale.setTo(0.4, 0.4);
-		// medicineBox.inputEnabled = true;
-		medicineBox.input.enableDrag();
+	// var shelterBox = game.add.image(game.world.width*18/20, game.world.height*5.5/20, 'shelterBox');
+	shelterBox.anchor.setTo(0.5, 0.5);
+	shelterBox.scale.setTo(0.4, 0.4);
+	// shelterBox.inputEnabled = true;
+	shelterBox.input.enableDrag();
+	// this.shelterBox.events.onDragStop.add(this.addShelter);
 
-		// var shelterBox = game.add.image(game.world.width*18/20, game.world.height*5.5/20, 'shelterBox');
-		shelterBox.anchor.setTo(0.5, 0.5);
-		shelterBox.scale.setTo(0.4, 0.4);
-		// shelterBox.inputEnabled = true;
-		shelterBox.input.enableDrag();
+	group.onChildInputOver.add(this.onOver, this);
+	group.onChildInputOut.add(this.onOut, this);
+	group.onChildInputDown.add(this.onDown, this);
 
-		group.onChildInputOver.add(this.onOver, this);
-		group.onChildInputOut.add(this.onOut, this);
-		group.onChildInputDown.add(this.onDown, this);
-
-	};
 
 	this.onOver = function (sprite) {
 		sprite.alpha = 1;
@@ -137,6 +138,40 @@ var Items = function (game, x, y, frame) {
 	};
 	this.onDown = function (sprite) {
 		sprite.tint = 0xffffff;
+	};
+
+	this.addFood = function (sprite) {
+		// if (checkOverlap(sprite.foodBox, game.state.states.play.transports.plane)) {
+		// 	console.log('On Top');
+		// }
+		// else {
+		// 	console.log('No Overlap');
+		// };
+	};
+
+	this.addWater = function (sprite) {
+		if (sprite.y < 600) {
+			console.log('Add water!');
+			console.log(sprite.getBounds());
+		}
+	};
+
+	this.addMedicine = function (sprite) {
+		if (true) {
+			console.log('Add Medicine');
+		}
+	};
+
+	this.addShelter = function (sprite) {
+		if (true) {
+			console.log('Add shelter!');
+		}
+	};
+
+	this.checkOverlap = function (spriteA, spriteB) {
+		boundsA = spriteA.getBounds();
+		boundsB = spriteB.getBounds();
+		return Phaser.Rectangle.intersects(this.boundsA, this.boundsB);
 	};
 
 };
@@ -221,7 +256,7 @@ var Transports = function (game, x, y, frame) {
 		game.state.states.play.camp.medicine += this.plane.medicine;
 		// game.state.states.play.camp.shelter += this.plane.shelter;
 
-		console.log(game.state.states.play.camp.food);
+		// console.log(game.state.states.play.camp.food);
 		
 	};
 
@@ -242,7 +277,7 @@ var Transports = function (game, x, y, frame) {
 		game.state.states.play.camp.shelter += this.porter.shelter;
 		game.state.states.play.camp.population += this.porter.population;
 
-		console.log(game.state.states.play.camp.population);
+		// console.log(game.state.states.play.camp.population);
 
 	};
 
@@ -252,53 +287,6 @@ var Transports = function (game, x, y, frame) {
 Transports.prototype = Object.create(Phaser.Sprite.prototype);
 
 module.exports = Transports;
-
-/**
-* This turns your displayObjects to grayscale.
-* @class Gray
-* @contructor
-*/
-Phaser.Filter.Gray = function (game) {
-
-    Phaser.Filter.call(this, game);
-
-    this.uniforms.gray = { type: '1f', value: 1.0 };
-
-    this.fragmentSrc = [
-
-        "precision mediump float;",
-
-        "varying vec2       vTextureCoord;",
-        "varying vec4       vColor;",
-        "uniform sampler2D  uSampler;",
-        "uniform float      gray;",
-
-        "void main(void) {",
-            "gl_FragColor = texture2D(uSampler, vTextureCoord);",
-            "gl_FragColor.rgb = mix(gl_FragColor.rgb, vec3(0.2126 * gl_FragColor.r + 0.7152 * gl_FragColor.g + 0.0722 * gl_FragColor.b), gray);",
-        "}"
-    ];
-
-};
-
-Phaser.Filter.Gray.prototype = Object.create(Phaser.Filter.prototype);
-Phaser.Filter.Gray.prototype.constructor = Phaser.Filter.Gray;
-
-/**
-* The strength of the gray. 1 will make the object black and white, 0 will make the object its normal color
-* @property gray
-*/
-Object.defineProperty(Phaser.Filter.Gray.prototype, 'gray', {
-
-    get: function() {
-        return this.uniforms.gray.value;
-    },
-
-    set: function(value) {
-        this.uniforms.gray.value = value;
-    }
-
-});
 },{}],5:[function(require,module,exports){
 'use strict'
 
@@ -387,16 +375,16 @@ module.exports = menuState;
 
 var playState = function (game) {
 	// console.log("Now starts the Play State!");
-}
+};
 
 var Camp = require('../obj/camp');
 var Warehouse = require('../obj/warehouse');
 var Items = require('../obj/items');
 var Transports = require('../obj/transports');
 
-var lives;
-var populationText;
-var scoreText;
+var lives = 3;
+var populationText = "";
+var scoreText = "";
 var gamePause = false;
 
 
@@ -433,8 +421,6 @@ playState.prototype = {
 
 		this.camp = new Camp(this.game, this.world.width/8, this.world.centerY);
 		this.game.add.existing(this.camp);
-
-
 		this.warehouse = new Warehouse(this.game, this.world.width*7/8, this.world.centerY);
 		this.game.add.existing(this.warehouse);
 
@@ -477,11 +463,6 @@ playState.prototype = {
 		populationText = this.add.bitmapText(this.world.width/20, this.world.height/20,'lastmileFont', 'Population: ' + this.camp.population, 20);
 		scoreText = this.add.bitmapText(this.world.width*15/20, this.world.height/20, 'lastmileFont', 'Scores: ' + this.scores, 20);
 		
-		// this.camp.expandStorage();
-		// this.warehouse.expandStorage();
-		// this.camp.showStock();
-		// this.warehouse.showStock();
-		
 
 		lives = this.add.group();
 		for (var i = 3; i > 0; i--) {
@@ -493,7 +474,7 @@ playState.prototype = {
 
 		this.items = new Items(this.game);
 		this.game.add.existing(this.items);
-		this.items.showBox();
+		// this.items.showBox();
 
 		// Time events: run the addPopulation function in every 1 minute;
 		// For testing purpose, the time is set to 1 seconds;
