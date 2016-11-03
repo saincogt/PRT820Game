040d926e5926955e99d400c4fdd1cf100e455b6e
Sancho <saincogt@gmail.com>
2016-11-03 14:50:04 +0930

    Changed UI of Play Button and Game Title
    Got rid of the Score Button as the score will be displayed in the end to
    the game and will not be displayed on the main menu screen.

--- build/js/main.js
+++ build/js/main.js
@@ -554,13 +554,15 @@ menuState.prototype = {
 		var title = this.game.add.sprite(this.world.centerX, this.world.centerY/2-20, 'title');
 		title.anchor.setTo(0.5, 0.5);
 		var tween = this.add.tween(title);
-		tween.to({y: this.world.width/5+25}, 800, Phaser.Easing.Bounce.Out, true, 500);
+		tween.to({y: this.world.height/2-50}, 800, Phaser.Easing.Bounce.Out, true, 500);
 
-		var startButton = this.game.add.button(this.world.centerX, 300, 'play', this.startPlay, this);
+		var startButton = this.game.add.button(this.world.centerX, this.world.centerY + 50, 'play', this.startPlay, this);
 		startButton.anchor.setTo(0.5, 0.5);
+		startButton.scale.setTo(1.2, 1.2);
 
-		var scoreButton = this.game.add.button(this.world.centerX, 360, 'score', this.checkScore, this);
-		scoreButton.anchor.setTo(0.5, 0.5);
+		// var scoreButton = this.game.add.button(this.world.centerX, 360, 'score', this.checkScore, this);
+		// scoreButton.anchor.setTo(0.5, 0.5);
+
 		var authorText = this.add.bitmapText(this.world.centerX, 500, 'lastmileFont', 'Developed by Sancho', 20);
 		authorText.anchor.setTo(0.5, 0.5);
 		this.game.add.button(this.world.width-50, this.world.height-50, 'fullscreen', this.fullScreen, this);
